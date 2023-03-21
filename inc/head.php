147fc259278ad13aa55baf547deef4b6777b8e51
神秘布偶猫 <kannafay@qq.com>
2023-03-21 17:10:41 +0800

    1.0.0

--- inc/head.php
+++ inc/head.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?=show_wp_title()?></title>
   <link rel="stylesheet" href="<?=file_url()?>/assets/css/style.css">
-  <link rel="stylesheet" href="//at.alicdn.com/t/c/font_3962262_ywk4mcaq8u.css">
+  <link rel="stylesheet" href="<?=file_url()?>/assets/static/iconfont/iconfont.css">
   <script src="<?=file_url()?>/assets/js/jquery.min.js"></script>
   <script src="<?=file_url()?>/assets/js/jquery.pjax.min.js"></script>
 </head>
